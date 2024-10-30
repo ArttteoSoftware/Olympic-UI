8be67fa212c7f3c3f6f3a913223b49a2529aa7cc
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-10-30 13:27:56 +0400

    refactoring card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import styles from "./Card.module.css";
 import { motion, Reorder } from "framer-motion";
 import axios from "axios";
@@ -11,47 +11,12 @@ import useSocketStore from "../../store/socketStore";
 import Grid from "../Grid/Grid";
 function Card({ title, units }) {
 	const [data, setData] = useState([]);
-	const [updatedData, setUpdatedData] = useState([]);
 	const [isFlipped, setIsFlipped] = useState(false);
-	const { dataState, unitCode } = useSocketStore();
+	const { dataState } = useSocketStore();
 
-	const loadData = useCallback(async () => {
-		try {
-			const response = await axios.get(
-				`${process.env.REACT_APP_API_URL}/players`
-			);
-			if (response.status === 200) {
-				setData(response.data);
-			}
-		} catch (error) {
-			console.error("Error loading initial data:", error);
-		}
-	}, []);
-
-	const loadUpdatedData = useCallback(async () => {
-		try {
-			const response = await axios.get(
-				`${process.env.REACT_APP_API_URL}/updatedPlayers`
-			);
-			if (response.status === 200) {
-				setUpdatedData(response.data);
-				setTimeout(() => {
-					setData(response.data);
-				}, 3000);
-			}
-		} catch (error) {
-			console.error("Error loading updated data:", error);
-		}
-	}, []);
-
-	// useEffect(() => {
-	// 	loadData();
-	// 	loadUpdatedData();
-
-	// 	const intervalId = setInterval(loadUpdatedData, 3000); // Update every 30 seconds
-
-	// 	return () => clearInterval(intervalId);
-	// }, [loadData, loadUpdatedData]);
+	useEffect(() => {
+		setData(dataState.current);
+	}, [dataState]);
 
 	const commonStyles = useMemo(
 		() => ({
@@ -66,32 +31,13 @@ function Card({ title, units }) {
 		[]
 	);
 
-	const flickerAnimation = {
-		flickering: {
-			backgroundColor: ["#ffffff", "#8fd5a5", "#ffffff", "#8fd5a5"],
-			transition: {
-				duration: 1,
-				repeat: Infinity,
-				repeatType: "reverse",
-				ease: "easeInOut",
-			},
-		},
-	};
-	const playerList = useMemo(() => {
-		return data.map((player, index) => {
-			const updatedIndex = updatedData.findIndex((p) => p.id === player.id);
-			const rankChange = updatedIndex !== -1 ? index - updatedIndex : 0;
-			return { ...player, rankChange };
-		});
-	}, [data, updatedData]);
-
 	return (
 		<div className={styles.mainContainer}>
 			<Reorder.Group
 				style={commonStyles}
 				animate={{ rotateY: isFlipped ? 180 : 0 }}
 				transition={{ duration: 0.6 }}
-				values={playerList}
+				values={data || []}
 				onReorder={setData}
 				className={styles.container}
 			>
@@ -113,7 +59,7 @@ function Card({ title, units }) {
 									<Grid
 										details={false}
 										columns={BiathlonCol}
-										data={unit.start_list || dataState.current}
+										data={data || dataState.current}
 										className={styles.cardGrid}
 									/>
 								</div>
