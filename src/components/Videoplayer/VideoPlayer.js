031e2981416e7bf73a50c9c6da142b9c1ec356df
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2024-11-26 00:12:57 +0400

    title is fixed

--- src/components/Videoplayer/VideoPlayer.js
+++ src/components/Videoplayer/VideoPlayer.js
@@ -6,42 +6,53 @@ const VideoPlayer = ({ onVideoEnd }) => {
 	const videoRef = useRef(null);
 
 	useEffect(() => {
+		let hls;
 		if (Hls.isSupported()) {
-			const hls = new Hls();
+			hls = new Hls();
 			hls.loadSource(`${process.env.REACT_APP_API_URL}hls/stream.m3u8`);
 			hls.attachMedia(videoRef.current);
 			hls.on(Hls.Events.MANIFEST_PARSED, () => {
-				if (!videoRef.current.paused) {
-					videoRef.current.play();
-				}
+				videoRef.current.play();
 			});
 		} else if (
 			videoRef?.current?.canPlayType("application/vnd.apple.mpegurl")
 		) {
 			videoRef.current.src = `${process.env.REACT_APP_API_URL}hls/stream.m3u8`;
 			videoRef.current.addEventListener("loadedmetadata", () => {
-				if (!videoRef.current.paused) {
-					videoRef.current.play();
-				}
+				videoRef.current.play();
 			});
 		}
+
+		// Cleanup function to destroy HLS instance
+		return () => {
+			if (hls) {
+				hls.destroy();
+			}
+		};
 	}, []);
 
 	useEffect(() => {
 		const videoElement = videoRef?.current;
 
+		const handleVideoError = () => {
+			console.error("Video playback error occurred.");
+			// Handle error (e.g., show a message to the user)
+		};
+
+		videoElement.addEventListener("error", handleVideoError);
+
 		const handleVideoEnd = () => {
-			if (onVideoEnd) {
-				onVideoEnd();
-			}
+			console.log("***!");
+			onVideoEnd();
 		};
 
 		videoElement.addEventListener("ended", handleVideoEnd);
 
 		return () => {
+			videoElement.removeEventListener("error", handleVideoError);
 			videoElement.removeEventListener("ended", handleVideoEnd);
 		};
-	}, [onVideoEnd]);
+	}, []);
 
 	return <video className={styles.video} ref={videoRef} muted />;
 };
